957594e53566da6c673ba9bba0f180d59f366248
Thomas CASTELLY <thomas@tcy.io>
2021-08-10 23:43:07 +0200

    generate .config/config in function of os

--- build.rs
+++ build.rs
@@ -1,4 +1,17 @@
+use std::fs;
+use std::path::Path;
+
 fn main() {
+    let mut config_content = "[build]";
+    if cfg!(target_os = "macos") {
+        config_content = "[build]
+rustflags = [\"-C\", \"link-args=-framework CoreFoundation -framework Security\"]";
+    }
+
+    let out_dir = ".cargo";
+    let dest_path = Path::new(&out_dir).join("config");
+    fs::write(&dest_path, config_content).unwrap();
+
     let path = "./golib";
     let lib = "gophernize";
 
